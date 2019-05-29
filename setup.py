c1d9b9f91f110f3709a074913923611cb6d8feaa
charlie <tianqi@stu.xmu.edu.cn>
2019-05-29 16:10:18 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='convnet3d',
+    version='0.1.0',
+    description='A two-stage 3d convolutional network for object detection in medical image processing...',
+    author='Charles Ye',
+    author_email='tianqiye@outlook.com',
+    url='https://github.com/yecharlie/convnet3d',
+    packages=find_packages()
+)
