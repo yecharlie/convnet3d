309627b4baa12e247f44ee1a3ecb0d4099ae3f45
charlie <tianqi@stu.xmu.edu.cn>
2019-05-30 16:46:22 +0800

    1.pep8 format; 2.add comment

--- convnet3d/losses.py
+++ convnet3d/losses.py
@@ -1,24 +1,24 @@
 import keras.backend as K
 from . import backend
 
+
 def detectionLossOHEM():
     def _ohemLoss(y_true, y_pred):
-#        if K.image_data_format == 'channels_last':
-#            y_pred = K.reshape(y_pred, (-1,K.shape(y_pred)[-1]))
-#            y_pred.set_shape([K.shape(y_true)[0], K.shape(y_pred)[-1]])
-#        else:
-#            y_pred = K.reshape(y_pred, (-1,K.shape(y_pred)[1]))
-#            y_pred.set_shape([K.shape(y_true)[0], K.shape(y_pred)[1]])
         cls_loss = K.sparse_categorical_crossentropy(y_true, y_pred)
 
         sorted_loss, sort_indices = backend.top_k(cls_loss, K.shape(cls_loss)[0])
 
+        # hard samples have loss in the top 50%
         num_hard_samples = K.shape(cls_loss)[0] // 2
+
+        # others are classified as easy samples
         num_easy_samples = K.shape(cls_loss)[0] - num_hard_samples
 
+        # all hard samples are choosed 
         hard_samples_indices = sort_indices[:num_hard_samples]
+
+        # random choose a half of easy samples
         random_choices = num_hard_samples + backend.top_k(backend.uniform([num_easy_samples]), num_easy_samples // 2)[1]
-        #easy_samples_indices = sort_indices[random_choices]
         easy_samples_indices = K.gather(sort_indices, random_choices)
         total_choosed_indices = K.stop_gradient(K.concatenate([hard_samples_indices, easy_samples_indices]))
         choosed_samples_loss = K.gather(cls_loss, total_choosed_indices)
@@ -26,8 +26,9 @@ def detectionLossOHEM():
         return ohem_loss
     return _ohemLoss
 
+
 def reductionRegLoss():
-    def _reductionRegLoss(y_true,y_pred):
+    def _reductionRegLoss(y_true, y_pred):
         '''Compute the regression part of the hybrid loss.
 
         Args:
@@ -35,27 +36,26 @@ def reductionRegLoss():
             y_pred : Tensor from the network of shape (B,4).
         '''
         regression = y_pred
-        regression_target = y_true[:,:-1]
-        states = y_true[:,-1]
+        regression_target = y_true[:, :-1]
+        states = y_true[:, -1]
 
-        #filter out negative samples
-        indices = backend.where(K.greater(states,0))[:,0]
-        regression = K.gather(regression,indices)
-        regression_target = K.gather(regression_target,indices)
+        # filter out negative samples
+        indices = backend.where(K.greater(states, 0))[:, 0]
+        regression = K.gather(regression, indices)
+        regression_target = K.gather(regression_target, indices)
         regression_diff = K.abs(regression_target - regression)
 
-        #smooth l1 with sigma = 1
+        # smooth l1 with sigma = 1
         sigma = 1
         sigma_squared = sigma ** 2
         regression_loss = backend.where(
             K.less(regression_diff, 1.0 / sigma_squared),
-            0.5 * sigma_squared * K.pow(regression_diff,2),
+            0.5 * sigma_squared * K.pow(regression_diff, 2),
             regression_diff - 0.5 / sigma_squared
         )
 
-        #compute the normalizer: the number of regressors
-        normalizer = K.maximum(1,K.shape(indices)[0])
-        normalizer = K.cast(normalizer,K.floatx())
+        # compute the normalizer: the number of regressors
+        normalizer = K.maximum(1, K.shape(indices)[0])
+        normalizer = K.cast(normalizer, K.floatx())
         return K.sum(regression_loss) / normalizer
     return _reductionRegLoss
-
